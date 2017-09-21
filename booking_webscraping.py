c2fb2f2a1b6dea567b438fb4db5104ca9877f772
chiara <chiasalv90@gmail.com>
2017-09-21 16:55:01 +0200

    update: loop correction + price correction + data type correction for db

--- booking_webscraping.py
+++ booking_webscraping.py
@@ -7,7 +7,7 @@ import requests
 import re
 import psycopg2
 import argparse
-from datetime import datetime
+import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--scraping_type", action="count", default=0)
@@ -20,7 +20,15 @@ base_url='http://www.booking.com'
 def crawler(location,day_in,day_out):
 
 #for the specific location,day_in(yyy-mm-dd),day_out(yyyy-mm-dd) return a list of class object
+    
     dest_id='-110502'
+    
+    monthday_in=day_in.day
+    month_in=day_in.month
+    year_in=day_in.year
+    monthday_out=day_out.day
+    month_out=day_out.month
+    year_out=day_out.year
     
     #setting random user agent
     #ua = UserAgent()
@@ -33,13 +41,14 @@ def crawler(location,day_in,day_out):
     headers={'User-Agent': ua.random}
     print (headers)
     
+    hotel_list=[]
   
     #loop  over all the pages containing research results iterating on the URL offset parameter
     #loop  stops when it finds error message "take control of your search"
 
     for i in range(0,1): #set the number of visited pages 
-        offset=i*30
-        url='https://www.booking.com/searchresults.it.html?;sid=1542c45b9ca387a9d5dc25d864e1ee17;&checkin_monthday=20&checkin_month=9;checkin_year=2017;checkout_monthday=21;checkout_month=9;checkout_year=2017;dest_id='+dest_id+';ss='+location+';dest_type=city;sb_travel_purpose=leisure;no_rooms=1;group_adults=2;group_children=0;offset='+str(offset)
+        offset=i*15
+        url='https://www.booking.com/searchresults.it.html?;sid=1542c45b9ca387a9d5dc25d864e1ee17;&checkin_monthday='+str(monthday_in)+'&checkin_month='+str(month_in)+';checkin_year='+str(year_in)+';checkout_monthday='+str(monthday_out)+';checkout_month='+str(month_out)+';checkout_year='+str(year_out)+';dest_id='+dest_id+';ss='+location+';dest_type=city;sb_travel_purpose=leisure;no_rooms=1;group_adults=2;group_children=0;offset='+str(offset)
                 
         print (url)
 
@@ -68,23 +77,22 @@ def crawler(location,day_in,day_out):
             soup = bs(response,'lxml')#.encode('utf-8')
    
             #script=soup.find_all('script', type_='application/ld+jso')
-            text_file = open("Output_soup.txt", "w")
-            text_file.write(str(soup))
-            text_file.close()
+            #text_file = open("Output_soup.txt", "w")
+            #text_file.write(str(soup))
+            #text_file.close()
+
+    
 
             if args.scraping_type >= 2:
-                print("type of scraping: hotel_table_update")
-                hotel_table_list=[]
                 hotel_table=hotel_table_update(hotel_link,soup)
-                hotel_table_list.append(hotel_table)
-                return hotel_table_list
+                hotel_list.append(hotel_table)
+            
  
             if args.scraping_type < 2:   
-                print("type of scraping: hotel_data_update ")
-                hotel_data_list=[]
                 hotel_data=hotel_data_update(day_in,day_out,hotel_link,soup)
-                hotel_data_list.append(hotel_data)
-                return hotel_data_list
+                hotel_list.append(hotel_data)
+        
+    return hotel_list
 
 ##################################################################################################
 
@@ -98,11 +106,9 @@ def hotel_table_update(hotel_link,hotel_soup) :
             self.address=address
             self.dest_id=dest_id
 
-    hotel_id=hotel_soup.find("input", {"name":"hotel_id"})['value']
+    hotel_id=int(hotel_soup.find("input", {"name":"hotel_id"})['value'])
                
-    dest_id=hotel_link[hotel_link.find('dest_id=')+len('dest_id='):hotel_link.rfind(';srfid')]
-
-    #add hotel_id=hotel.soup.........
+    dest_id=int(hotel_link[hotel_link.find('dest_id=')+len('dest_id='):hotel_link.rfind(';srfid')])
 
     if hotel_soup.find('h2', class_='hp__hotel-name'):
         hotel_name=hotel_soup.find('h2', class_='hp__hotel-name').text.strip('\t\r\n')
@@ -131,18 +137,25 @@ def hotel_data_update (day_in,day_out,hotel_link,hotel_soup):
             self.day_out=day_out
             self.price=price
             self.av_rating=av_rating
-            self.search_date=str(datetime.now())
+            self.search_date=(datetime.datetime.now())
 
-    hotel_id=hotel_soup.find("input", {"name":"hotel_id"})['value']
+    hotel_id=int(hotel_soup.find("input", {"name":"hotel_id"})['value'])
 
     av_rating=hotel_soup.find('span', class_='review-score-badge').text.strip('\t\r\n')
-    #av_rating=float(re.sub('[^0-9,]', "", av_rating).replace(",", "."))
+    av_rating=float(re.sub('[^0-9,]', "", av_rating).replace(",", "."))
     print(av_rating)
-    if hotel_soup.find('span', class_='hprt-price-price-standard'):
-        price=hotel_soup.find('span', class_='hprt-price-price-standard')#.select('b')
+    if hotel_soup.select_one('strong.js-track-hp-rt-room-price'):
+        price=hotel_soup.select_one('strong.js-track-hp-rt-room-price') 
         price = price.text.strip('\t\r\n\€xa')
         price=float(re.sub('[^0-9,]', "", price).replace(",", "."))
+    elif hotel_soup.find('span', class_='hprt-price-price-standard'):
+         price=hotel_soup.find('span', class_='hprt-price-price-standard')
+         price = price.text.strip('\t\r\n\€xa')
+         price=float(re.sub('[^0-9,]', "", price).replace(",", "."))
     else :
+        text_file = open("soup_error_price.txt", "w")
+        text_file.write(str(hotel_soup))
+        text_file.close()
         price= None
     print(price)       
     
@@ -194,7 +207,7 @@ def db_hotel_data_update(hotel_data_list) :
     
         SQL = '''BEGIN;
                  INSERT INTO hotel_data (hotel_id,day_in,day_out,price,av_rating,search_date)
-	         VALUES  %s,%s,%s,%s,%s,%s 
+	         VALUES (%s,%s,%s,%s,%s,%s) 
                  ;
                  COMMIT;'''
 
@@ -208,30 +221,26 @@ def db_hotel_data_update(hotel_data_list) :
 ####################################################################################
 
 #research_options
-day_in='2017-09-20'
-day_out='2017-09-21'
+day_in_str='2017-09-21'
+day_out_str='2017-09-22'
 
 
 
+day_in = datetime.datetime.strptime(day_in_str,'%Y-%m-%d')
+day_out= datetime.datetime.strptime(day_out_str, '%Y-%m-%d')
 
 if args.scraping_type >= 2:
+    print("type of scraping: hotel_table_update")
     hotel_table_list=crawler('Aosta',day_in,day_out)
-    print(hotel_table_list)
     #db updating hotel_list in webcraling postgress db
     db_hotel_list_update(hotel_table_list)
 
 if args.scraping_type < 2:   
+    print("type of scraping: hotel_data_update ")
     hotel_data_list=crawler('Aosta',day_in,day_out)
     #db updating hotel_data in webcrawling postgress db
     db_hotel_data_update(hotel_data_list)
 
-
-
-#for script in scripts:
-#   if(pattern.match(str(script.string))):
-#       data = pattern.match(script.string)
-#       stock = json.loads(data.groups()[0])
-#       print stock
 
 
 
