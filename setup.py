9140dd6fbe4628d778a6c04bcebdccb3eff2ea98
chiara <chiasalv90@gmail.com>
2017-11-17 10:16:15 +0100

    update

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-from setuptools import setup, find_packages
-setup(
-    name="booking_list_scraping",
-    version="3.14",
-    packages=find_packages(),
-    scripts=['booking_list_scraping.py','conn_db.py'],
+from distutils.core import setup
 
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
-    install_requires=['BeautifulSoup','fake_useragent','requests','psycopg2','argparse'],
+setup(name='booking_list_scraping',
+      version='3.14',
+      author='Greg Ward',
+      #author_email='',
+      url='https://github.com/chiarabs/far2-webcrawling',
+      packages=[],
+      py_modules=['booking_list_scraping','conn_db'],
+      data_files=[('', ['hotel_link_list.csv','search_opt.txt'])],
+      requires=['BeautifulSoup','fake_useragent','requests','psycopg2','argparse']
+     )
 
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['hotel_link_list.csv','search_opt.txt'],
-        # And include any *.msg files found in the 'hello' package, too:
-      #  'hello': ['*.msg'],
-    })
 
-    # metadata for upload to PyPI
-    #author="",
-    #author_email="",)
-    #description="This script allows to scrape booking.com hotel data and collect them in a structured way using a psql db",
+
+
